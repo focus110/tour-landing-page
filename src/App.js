487f1ffeedefb80fa57d9e3cd65fd45d48c19c26
focus110 <iamemmaochuko@gmail.com>
2024-03-29 23:35:35 +0100

    added topbar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import "./App.css";
 import Navbar from "./components/Navbar";
+import Topbar from "./components/Topbar";
 
 function App() {
   return (
     <div className="App">
+      <Topbar />
       <Navbar />
     </div>
   );
---
+++ src/components/Topbar.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Topbar = () => {
+  return (
+    <div className="container bg-secondary item text-[14px] 3xl:text-[18px] p-2 text-accent-red font-semibold">
+      10% OFF ESSENTIAL TICKET!
+    </div>
+  );
+};
+
+export default Topbar;
